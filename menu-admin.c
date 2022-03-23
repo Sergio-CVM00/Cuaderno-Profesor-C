abab86b6e597f6aa383768c5be9d3c2bf503ef29
Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
2022-03-23 13:21:52 +0100

    Squashed commit of the following:
    commit 84adc6744a13867fa9dc4ceceb39b64c279accd7
    Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
    Date:   Wed Mar 23 13:18:48 2022 +0100
    
        update menu-admin.c expliacion github

--- menu-admin.c
+++ menu-admin.c
@@ -1,1 +1,7 @@
 //test git sergio
+//He modificado desde MI RAMA, hago un commit de los cambios que he hecho.
+//Pusheo esos cambios a MI RAMA para tenerlos guardado en github.
+
+//Cuando quieras llevar los cambios de ese fichero a la rama MAIN.
+//Dentro de github desktop te colocas en la rama main, vas a la opcion de "branch/rama"
+//PULSAS "merge into current branch" seleccionas la rama de la cual quieras traer esos cambios.
