b6105bb8755c04c131247fb959fb5807a55f2fd3
robelora <roberto.loramontero@alum.uca.es>
2022-03-24 11:41:25 +0100

    Dar de alta y baja alumnos
    CON ERRORES

--- menu-admin.c
+++ menu-admin.c
@@ -5,30 +5,69 @@
 //Cuando quieras llevar los cambios de ese fichero a la rama MAIN.
 //Dentro de github desktop te colocas en la rama main, vas a la opcion de "branch/rama"
 //PULSAS "merge into current branch" seleccionas la rama de la cual quieras traer esos cambios.
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <ctime>
+#include "menu-admin.h"
 
 void usuarios_alta()
 
+void alumnos_alta(alumno *vAlumno, int *nAlumno){
+int menuAnadir,i,j,auxiliar1,auxiliar2;
+   do {
+            printf("\nSeleccione una opcion:\n");
+            printf("1: Dar de alta Alumno \n");
+            printf("2: Dar de baja Alumno\n");
+            fflush(stdin);
+            scanf("%i", &menuAnadir);
+              }while(menuAnadir<1 || menuAnadir>2);
+    switch (menuAnadir){
 
-void AnadirUsuarios(usuario *vUsuario, int *nUsuario){
+        case 1:
+            fflush(stdin);
+            printf("\nIntroduce el identificador del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].id_alum);
+            fflush(stdin);
+            printf("\nIntroduce el nombre del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].nombre_alum);
+            fflush(stdin);
+            printf("\nIntroduce la direccion del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].direc_alum);
+            fflush(stdin);
+            printf("\nIntroduce la localidad del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].local_alum);
+            fflush(stdin);
+            printf("\nIntroduce el curso del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].curso);
+            printf("\nIntroduce el grupo del nuevo alumno\n");
+            gets((vAlumno)[*nAlumno].grupo);
+            (*nAlumno)++;
+            printf("Correctamente anadido\n");
 
+        case 2:
+            printf("\nIntroduce el identificador del alumno que desea eliminar\n");
+            gets(eliminarA);
+            for(j=0;j<(*nAlumno);j++){
+            if(strcmp((vAlumno)[j].id_alum,eliminarA)==0){
+            auxiliar2=j;
+                }
+            }
 
-     fflush(stdin);
-     printf("\nIntroduce el identificador del nuevo usuario\n");
-	 gets((vUsuario)[*nUsuario].id_usuario);
-	 fflush(stdin);
-     printf("\nIntroduce el nombre del nuevo usuario\n");
-	 gets((vUsuario)[*nUsuario].nombre);
-	 fflush(stdin);
-     printf("\nIntroduce el perfil del nuevo usuario\n");
-	 gets((vUsuario)[*nUsuario].Rol);
-	 fflush(stdin);
-     printf("\nIntroduce el nombre de USUARIO del nuevo usuario\n");
-	 gets((vUsuario)[*nUsuario].nick);
-	 fflush(stdin);
-     printf("\nIntroduce la contrasena del nuevo usuario\n");
-	 gets((vUsuario)[*nUsuario].contrasena);
-     (*nUsuario)++;
-     printf("Correctamente anadido\n");
+            for(j=auxiliar2;j+1<(*nAlumno);j++){ ///////////////////////////////////////////////////////
+            strcpy((vAlumno)[j].nombre_alum,(vAlumno)[j+1].nombre_alum);
+            strcpy((vAlumno)[j].direc_alum,(vAlumno)[j+1].direc_alum);
+            strcpy((vAlumno)[j].local_alum,(vAlumno)[j+1].local_alum);
+            strcpy((vAlumno)[j].curso,(vAlumno)[j+1].curso);
+            strcpy((vAlumno)[j].grupo,(vAlumno)[j+1].grupo);
+           //PARA ENTEROS (vAlumno)[j].precio=(vAlumno)[j+1].precio;
+            
 
-    }
+            (*nAlumno)--;
+            printf("\nEl Alumno se elimino correctamente\n");
+        }
+            
+     
+    
+
+     }
