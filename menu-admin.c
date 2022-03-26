54cada8fe9435fa0720f7be61b178fa8fcc88f34
robelora <roberto.loramontero@alum.uca.es>
2022-03-26 15:54:58 +0100

    Squashed commit of the following:
    commit 7a8fa9f82d19319ab0c6b7102b059f9c1bde3052
    Author: robelora <roberto.loramontero@alum.uca.es>
    Date:   Sat Mar 26 15:17:22 2022 +0100
    
        poca cosa
    
    commit b64d94d337a78074f0278ac9bbd2d105766e062c
    Author: robelora <roberto.loramontero@alum.uca.es>
    Date:   Sat Mar 26 12:31:50 2022 +0100
    
        FUNCIONES DE MATERIA
    
    commit f9aa5fd0f9c37b1de83418110a2119f787a8bc0b
    Author: robelora <roberto.loramontero@alum.uca.es>
    Date:   Fri Mar 25 15:27:59 2022 +0100
    
        CAMBIOS SIN IMPORTANCIA
    
    commit 8cbe38e3c9cf83b218b214a96c0fb98d04b6a939
    Author: robelora <roberto.loramontero@alum.uca.es>
    Date:   Fri Mar 25 15:02:12 2022 +0100
    
        FUNCION ALUMNOS
    
        DAR DE ALTA,DAR DE BAJA, MODIFICAR Y LISTAR
    
    commit 22e6d3511e406acb8e0cac01319299e7a7e89508
    Author: robelora <roberto.loramontero@alum.uca.es>
    Date:   Thu Mar 24 10:31:29 2022 +0100
    
        Squashed commit of the following:
    
        commit 8b815038a56d5998ca776bddbad368fc8b5be770
        Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
        Date:   Thu Mar 24 10:00:18 2022 +0100
    
            Distribuir structs(.h) en profesor y admin
    
        commit abab86b6e597f6aa383768c5be9d3c2bf503ef29
        Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
        Date:   Wed Mar 23 13:21:52 2022 +0100
    
            Squashed commit of the following:
    
            commit 84adc6744a13867fa9dc4ceceb39b64c279accd7
            Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
            Date:   Wed Mar 23 13:18:48 2022 +0100
    
                update menu-admin.c expliacion github
    
        commit 10ddfa2a23ea1c0549e9098569119062ef61ccd3
        Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
        Date:   Wed Mar 23 13:08:45 2022 +0100
    
            Update menu-admin.c
    
        commit 71bed908f829d2c89ef121d3cd15c6b19cbb2505
        Author: Sergio Cabeza de Vaca Montero <sergiomontero1969@gmail.com>
        Date:   Wed Mar 23 12:49:49 2022 +0100
    
            5 módulos
    
            Creación de los ficheros necesarios para la nueva modularidad descrita en documentación. ( .h y .c )

--- menu-admin.c
+++ menu-admin.c
@@ -34,9 +34,10 @@ char eliminarA[2];
             printf("2: Dar de baja Alumno\n");
             printf("3: Modificar Alumno\n");
             printf("4: Listar Alumnos\n");
+            printf("5: Informacion de alumnos\n");
             fflush(stdin);
             scanf("%i", &menuAnadir);
-              }while(menuAnadir<1 || menuAnadir>2);
+              }while(menuAnadir<1 || menuAnadir>5);
     switch (menuAnadir){
 
         case 1:
@@ -101,7 +102,7 @@ char eliminarA[2];
                     printf("6: Cambiar grupo del alumno\n");
                         fflush(stdin);
                         scanf("%i",&d);
-                    }while(d<1 || d>5);
+                    }while(d<1 || d>6);
 
                     switch(d){
                     case 1:
@@ -157,7 +158,7 @@ char eliminarA[2];
             printf("Los alumnos y su informacion son: \n\n");
             for(i=0;i<(*nAlumno);i++){
 
-            printf("Identificador del usuario: %s\n",(vAlumno)[i].id_alum);
+            printf("Identificador del usuario: %d\n",(vAlumno)[i].id_alum);
             printf("Nombre del alumno: %s\n",(vAlumno)[i].nombre_alum);
             printf("Direccion del alumno: %s\n",(vAlumno)[i].direc_alum);
             printf("Localidad del alumno: %s\n",(vAlumno)[i].local_alum);
@@ -168,12 +169,127 @@ char eliminarA[2];
                 }
 
             puts("////////////////////////////////////////////////////////////////////////////////////////////////////");
+        case 5:
+        /*Además, para un alumno seleccionado, se permitirá mostrar la lista de materias en las que se encuentra matriculado, 
+        realizar cambios de matrícula a otras materias, eliminar matrícula en alguna materia y crear nuevas matrículas. */
+        
   }
 }
-/*Además, para un alumno seleccionado, se permitirá mostrar la lista de materias en las que se encuentra matriculado, 
-realizar cambios de matrícula a otras materias, eliminar matrícula en alguna materia y crear nuevas matrículas. */
 
 
+/*MATERIAS*/
+void materias_funciones(materia *vMateria, int *nMateria){
+    int menuAnadir,i,j,auxiliar1,auxiliar2;
+char eliminarM[2];
+   do {
+            printf("\nSeleccione una opcion:\n");
+            printf("1: Dar de alta Materia \n");
+            printf("2: Dar de baja Materia\n");
+            printf("3: Modificar Materia\n");
+            printf("4: Listar Materia\n");
+            fflush(stdin);
+            scanf("%i", &menuAnadir);
+              }while(menuAnadir<1 || menuAnadir>4);
+    switch (menuAnadir){
+
+        case 1:
+            fflush(stdin);
+            printf("\nIntroduce el identificador de la nueva materia\n");
+            gets((vMateria)[*nMateria].id_materia);
+            fflush(stdin);
+            printf("\nIntroduce el nombre del nuevo alumno\n");
+            gets((vMateria)[*nMateria].nombre_materia);
+            fflush(stdin);
+            printf("\nIntroduce la direccion del nuevo alumno\n");
+            gets((vMateria)[*nMateria].abrev_materia);
+            fflush(stdin);
+            (*nMateria)++;
+            printf("Correctamente anadida\n");
+
+        case 2:
+            printf("\nIntroduce el identificador de la materia que desea eliminar\n");
+            gets(eliminarM);
+            for(j=0;j<(*nMateria);j++){
+            if(strcmp((vMateria)[j].id_materia,eliminarM)==0){
+            auxiliar2=j;
+                }
+            }
+
+            for(j=auxiliar2;j+1<(*nMateria);j++){ ///////////////////////////////////////////////////////
+            strcpy((vMateria)[j].nombre_materia,(vMateria)[j+1].nombre_materia);
+            strcpy((vMateria)[j].abrev_materia,(vMateria)[j+1].abrev_materia);
+           //PARA ENTEROS (vAlumno)[j].precio=(vAlumno)[j+1].precio;
+            
+
+            (*nMateria)--;
+            printf("\nLa materia se elimino correctamente\n");
+        }
+        case 3:
+            int i,d,correctoU=0;
+            char a[2], nuevo_idM[3],nuevo_nombreM[49],nueva_abrevM[3];
+            printf("Seleccione el identificador de la materia en el que quiere hacer cambios\n\n");
+
+            gets(a);
+
+            for(i=0;i<(*nMateria);i++){
+                if(strcmp((vMateria)[i].id_materia,a)==0)
+                correctoU=1;
+                do{
+                    printf("Seleccione una opcion\n");
+                    printf("1: Cambiar identificador de la materia\n");
+                    printf("2: Cambiar nombre de la materia\n");
+                    printf("3: Cambiar abreviatura de la materia\n");
+                        fflush(stdin);
+                        scanf("%i",&d);
+                    }while(d<1 || d>3);
+
+                    switch(d){
+                    case 1:
+                        printf("Introduce el nuevo identificador del alumno\n");
+                        fflush(stdin);
+                        gets(nuevo_idM);
+                        strcpy((vMateria)[i].id_materia,nuevo_idM);
+                        break;
+
+                    case 2:
+                        printf("Introduce el nuevo nombre de la materia\n");
+                        fflush(stdin);
+                        gets(nuevo_nombreM);
+                        strcpy((vMateria)[i].nombre_materia,nuevo_nombreM);
+                        break;
+
+                    case 3:
+                        printf("Introduce la nueva abreviatura para la materia\n");
+                        fflush(stdin);
+                        gets(nueva_abrevM);
+                        strcpy((vMateria)[i].abrev_materia,nueva_abrevM);
+                        break;
+
+                    }
+                   
+
+                    }
+               
+        case 4:
+            int i;
+            printf("Se mostraran todos las materias del sistema:\n\n");
+            puts("//////////////////////////////////////////////////////////////////////////////");
+            printf("Los alumnos y su informacion son: \n\n");
+            for(i=0;i<(*nMateria);i++){
+
+            printf("Identificador de la materia: %d\n",(vMateria)[i].id_materia);
+            printf("Nombre de la materia: %s\n",(vMateria)[i].nombre_materia);
+            printf("Abreviatura de la materia: %s\n",(vMateria)[i].abrev_materia);
+            puts("/////////////////////////////////////////////////////////////////////////////");
+
+                }
+
+            puts("////////////////////////////////////////////////////////////////////////////////////////////////////");
+  }
+
+
+
+}
             
      
     
