c71b6ae5bbb699beb0217811d4520f7e6be16cf9
MiguelAlfaroBoy <102228132+MiguelAlfaroBoy@users.noreply.github.com>
2022-03-29 13:23:42 +0200

    Add files via upload

--- menu.c
+++ menu.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "cargar-guardar.h"
+#include "usuario.h"
 
+void registro(usuario **vUsuario,int *nUsuario);
+int log_in(usuario *vUsuario, int nUsuario, char *id_usr);
 
-int main(){
-  //FUNCIONES DE CARGA, necesarias para el correcto uso de datos dentro del sistema.
-  int nUsuario; usuario *vUsuario = CargarUsuarios(vUsuario, &nUsuario);
-  printf("\nNumero de usuarios: %s\n", nUsuario);
-  return 0;
-}
-
-
-
-
-/*
 int main()
 {
-
+int nUsuario; usuario *vUsuario = CargarUsuarios(vUsuario, &nUsuario);
+int usuario;
 int op;
-int nUsuario;
-int usuario
-//int *vUsuario = CargarUsuarios(vUsuario, &nUsuario);
-
-
-
-
+char id_usr[2];  
   do
   {
-
   system("cls");
   printf("!Bienvenido al cuaderno del profesor� \n 1.Registro \n 2.Log-In \n 3.Salir \n");
   fflush(stdin);
@@ -37,52 +22,76 @@ int usuario
    if(op==1)
    registro(&vUsuario, &nUsuario);
    if(op==2)
-    log_in(  );
-  
+    usuario=log_in(vUsuario, nUsuario, id_usr);
    if(op==3)
     exit(-1);
-
   }while( op>3);
+
+
+
+//Aqui iran los menus de profesor y admin.
+
+
+
 }
-*/
-/*
-void registro(usuario **vUsuario, int *nUsuario)
+
+void registro(usuario **vUsuario,int *nUsuario)
 {
-  system("cls");
-  char prof[] = "profesor";
-  char num_char[3 + sizeof(char)];
-  typedef struct
+system("cls");
+  char usr[]="usuario";
+	char num_char[3 + sizeof(char)];
+	typedef struct
   {
     char nombre[20];
     char contrasena[9];
-  } provisional;
-  provisional provus;
+	}regist;
 
-  printf("Introduzca su nombre: ");
-  fflush(stdin);
-  gets(provus.nombre);
-  printf("\nIntroduzca su contrasena: ");
-  fflush(stdin);
-  gets(provus.contrasena);
-  printf("\nSu registro se ha realizado exito");
+  regist provus;
 
-  printf("\n Registro realizado con exito.");
-  printf("-%s", provus.nombre);
-  printf("-%d", *nUsuarios);
-  system("pause");
+		printf("Introduzca su nombre: ");
+        fflush(stdin);
+        gets(provus.nombre);
+        printf("\nIntroduzca su contrasena: ");
+        fflush(stdin);
+        gets(provus.contrasena);
+        printf("\nSu registro se ha realizado exito");
 
-  *vUsuario = (usuario *)realloc(*vUsuario, ((*nUsuarios) + 1) * sizeof(usuario));
+        printf("\n Registro realizado con exito.");
+        printf("-%s",provus.nombre);
+        printf("-%d",*nUsuario);
+        system("pause");
 
-  strcpy((*vUsuario)[*nUsuarios].nombre, provus.nombre);
-  strcpy((*vUsuario)[*nUsuarios].contrasena, provus.contrasena);
+        *vUsuario = (usuario *)realloc(*vUsuario, ((*nUsuario) + 1) * sizeof(usuario));
+        
+        strcpy((*vUsuario)[*nUsuario].Rol, usr);
+        strcpy((*vUsuario)[*nUsuario].nombre, provus.nombre);
+        strcpy((*vUsuario)[*nUsuario].contrasena, provus.contrasena);
 
-
-
+if(*nUsuario<10)
+    {
+        	(*vUsuario)[*nUsuario].id_usuario[2]='0';
+        	sprintf(num_char, "%d", *nUsuario+1);
+        	strcat((*vUsuario)[*nUsuario].id_usuario,num_char);
+		}
+		else
+    {
+		sprintf(num_char, "%d", *nUsuario+1);
+		strcpy((*vUsuario)[*nUsuario].id_usuario, num_char);		
+		}
+(*nUsuarios)++;
+printf("%d",*nUsuario);
+printf("-%s",vUsuario[4]->nombre);
+system("pause");
 }
-*/
-/*int log_in()
+
+int log_in(usuario *vUsuario, int nUsuario, char *id_usr)
 {
-system("cls");
+    int opcion;
+    char nombre[20];
+    int usuario_actual;
+    char c;
+    int t=0;
+    char contrasena[9];
 
   do{
        printf("\nIntroduzca su nick: ");
@@ -92,6 +101,32 @@ system("cls");
         fflush(stdin);
         gets(contrasena);
 
+
+        for(usuario_actual = 0; usuario_actual < nUsuario && t==0 ; usuario_actual++ )
+        {
+          if ((strcmp(vUsuario[usuario_actual].nombre, nombre) == 0) && (strcmp(vUsuario[usuario_actual].contrasena, contrasena) == 0))
+          {
+        	strcpy(id_usr,vUsuario[usuario_actual].id_usuario);
+
+    			if(strcmp(vUsuario[usuario_actual].Rol,"admin")==0)
+          {
+     			  vUsuario[usuario_actual].id_rol=1;	
+				  }
+    			if(strcmp(vUsuario[usuario_actual].Rol,"profesor")==0)
+          {
+       			vUsuario[usuario_actual].id_rol= 2;
+       		}
+       		t = 1;
+          }
+        }
+        if(t==0)
+         {
+          printf("El usuario o la contrasena son incorrectas. �Desea volver a intentarlo?s/n");
+          fflush(stdin);
+          scanf("%c",&c);
+         }
+        }while(c=='s');
+return usuario_actual-1;
   }
-}
-*/
+
+
