d7bb23c1be0cb3860a14ff5af2bb3c1e57265756
ALB3R70RT <102226809+ALB3R70RT@users.noreply.github.com>
2022-04-17 01:02:21 +0200

    Update menu-profesor.c

--- menu-profesor.c
+++ menu-profesor.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "menu-profesor.h"
-//#include "profesor.h"
+#include <stdlib.h>
+#include <math.h>
+#include "profesor.h"
 
-/*void MenuProfesor ( char* NombreProfesor, alumnos *valumnos , horario *vhorario, calificaciones  *vcalificaciones){
+void MenuProfesor (usuario *usu,alumno *alum  ,horario *hor,calificaciones *cali);
+void ficha_alumno(usuario *usu,alumno *alum,calificaciones *cali,int pos);
 
+
+
+
+
+
+void MenuProfesor ( usuario *usu, alumno *alum  ,horario *hor,calificaciones *cali){
     int x;
     printf("\n Bienvenido al  menu profesor, elige una de las siguientes \n");
 	do{
     printf("1: Listado de alumnos\n");
     printf("2: Horario\n");
-    printf("3: Calificaciones\n");
+    printf("3: Ficha alumno\n");
     printf("4: Salir del menu profesor \n");
     	fflush(stdin);
     scanf("%i",&x);
@@ -22,19 +29,82 @@
         case 1: printf("Listado de alumnos\n");
 				listar_alumnos();break;
         case 2: printf("Horario\n");
-				listar_horario();break;
-		case 3: printf("Calificaciones\n");
-				listar_calificaciones();break;
+				void mostrar_horario(usuario *usu,horario *hor,materia *mat, int pos)
+				{
+					printf("Ingrese su Id :\n");
+					scanf("%i",&pos);
+	
+					printf("1-Id  2-dia_clase  3-hora_clase  4-id_materia  5-nombre_materia\n");
+
+    				if (pos > 0 && pos < nUsuario)
+   					 {
+        				printf("\n%s/%i/%i/%i/%s\n",hor[pos].id_profesor, hor[pos].dia_clase, hor[pos].hora_clase,hor[pos].id_materia,mat[pos].nombre_materia);
+  					  }
+				};break;
+
+		case 3: printf("Ficha de alumno\n");
+				void ficha_alumno(usuario *usu ,alumno *alum,calificaciones *cali,int pos)
+				{
+						int y ;
+							printf("\n Bienvenido a la ficha de alumno");
+							do{
+							printf("1: Ficha\n");
+							printf("2: Calificaciones\n");
+							printf("3: Volver al menu profesor\n");
+							fflush(stdin);
+							scanf("%i",&y);
+							}while(y<1 || y>3);
+
+							switch(y){
+									case 1: printf("Ficha del alumno\n");
+											void mostrar_alumno(alumno *alum, int pos)
+											{
+
+	
+												printf("Seleccione el numero del aljmno que quiera ver en cuestion\n");
+												scanf("%i",&pos);
+	
+												printf("1-id  2-nombre_completo  3-direc_alum  4-curso  5-grupo\n");
+
+    											if (pos > 0 && pos < nAlumno)
+   												 {
+       												 printf("\n%i/%s/%s/%s/%s\n", alum[pos].id_alum, alum[pos].nombre_alum, alum[pos].direc_alum, alum[pos].local_alum, alum[pos].curso,alum[pos].grupo);
+  												  }
+											};break;
+		
+									case 2:	printf("Calificaciones\n");
+										void listar_calificaciones(alumno *alum, calificaciones *cali,int pos)
+										{
+											printf("Seleccione el numero del alumno que quiera ver en cuestion\n");
+											scanf("%i",&pos);
+	
+											printf("1-id  2-nombre_completo  3-calificacion");
+
+   											 if (pos > 0 && pos < nAlumno)
+   											 {
+       											 printf("\n%i/%s/%i\n", alum[pos].id_alum, alum[pos].nombre_alum,cali[pos].valor_cal );
+   											 }
+										};break;
+
+								}
+								system ("PAUSE");
+	
+								return ;
+
+	
+	
+};break;
+
+
+
     }
 
     printf("\nHa salido del menu profsor\n");
     system("PAUSE");
 
-return;
+return ;
 
 }
-
-
 
 
 
@@ -42,7 +112,6 @@ return;
 void listar_alumnos(){
 char aux;
 FILE *Alumnos;  
-
 Alumnos=fopen("Alumnos.txt","r");
 if (Alumnos == NULL)
 {
@@ -60,42 +129,3 @@ fclose(Alumnos);
 
 
 
-void listar_horarios(){
-char aux;
-FILE *Horario;  
-
-Horario=fopen("Horarios.txt","r");
-if (Horario == NULL)
-{
-    printf("Error al abrir el fichero");
-    exit(1);
-}
-
-while(aux != EOF) {
-    aux = fgetc(Horario);
-    printf("%c",aux);
-
-}
-fclose(Horario);
-}
-
-
-void listar_calificaciones(){
-char aux;
-FILE *Calificaciones;  
-
-Calificaciones=fopen("Calificaciones.txt","r");
-if (Calificaciones) == NULL)
-{
-    printf("Error al abrir el fichero");
-    exit(1);
-}
-
-while(aux != EOF) {
-    aux = fgetc(Calificacione);
-    printf("%c",aux);
-
-}
-fclose(Calificaciones);
-}
-*/
